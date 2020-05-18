dcd4c77a17d419cd1079296d534bd9ec71ea7bfe
Flebon <fabienlebon974@gmail.com>
2020-05-18 19:36:57 +0200

    feat(model) : Add Pin Model

---
+++ OnTheMap/Models/OTM Response/Pin.swift
@@ -0,0 +1,22 @@
+//
+//  Pin.swift
+//  OnTheMap
+//
+//  Created by Fabien Lebon on 18/05/2020.
+//  Copyright © 2020 Fabien Lebon. All rights reserved.
+//
+
+import Foundation
+
+struct Pin: Codable, Equatable {
+    let createdAt: String?
+    let firstName: String?
+    let lastName: String?
+    let latitude: Double?
+    let longitude: Double?
+    let mapString: String?
+    let mediaUrl: String?
+    let objectId: String?
+    let uniqueKey: String?
+    let updatedAt: String?
+}
---
+++ OnTheMap/Models/OTMClient.swift
@@ -0,0 +1,9 @@
+//
+//  OTMClient.swift
+//  OnTheMap
+//
+//  Created by Fabien Lebon on 18/05/2020.
+//  Copyright © 2020 Fabien Lebon. All rights reserved.
+//
+
+import Foundation
